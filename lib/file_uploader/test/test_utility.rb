101e3c89ab5d7cfbbaf4e166a2ebb4e5f043628f
Noah Chase <nchase@gmail.com>
2013-03-24 18:36:35 -0400

    add test fixture and fixture-reference for first test

--- lib/file_uploader/test/test_utility.rb
+++ lib/file_uploader/test/test_utility.rb
@@ -6,8 +6,10 @@ include FileUploader::Utility
 		#@resource = Resource.new("http://content.internetvideoarchive.com/content/photos/6894/28955235_.jpg")
 
 class TestLocal < Test::Unit::TestCase
+	FILENAME = "c43a8cbe52fecbe6fa25f0b85abb44f6_o.jpg"
+
 	def setup
-		file = File.open(File.expand_path('../../test/fixtures/test.jpeg', __FILE__))
+		file = File.open(File.expand_path("../../test/fixtures/#{FILENAME}", __FILE__))
 		@resource = Resource.new(file)
 	end
 	def test_uri
