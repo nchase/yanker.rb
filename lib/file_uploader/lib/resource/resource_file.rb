330558de1bbf2ab6821ce456711548b89d5b1b17
Noah Chase <nchase@gmail.com>
2013-04-29 09:54:23 -0400

    housekeeping

--- lib/file_uploader/lib/resource/resource_file.rb
+++ lib/file_uploader/lib/resource/resource_file.rb
@@ -1,19 +1,19 @@
 module FileUploader
 	class FileResource < Resource
-		def uri
-			self.path
-		end
-
-		def basename
-			File.basename(@file)
-		end
-
 		def initialize(resource)
 			super
 
 			FileUtils.copy(self.uri, self.basename)
 
 			self.tempfile = File.open(self.basename)
+		end
+
+		def uri
+			self.path
+		end
+
+		def basename
+			File.basename(@file)
 		end
 	end
 end
